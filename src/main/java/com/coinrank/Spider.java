876e770cf3ef1832e75b3c8b739f2bced22407a0
hadlow <me@billyhadlow.com>
2018-01-15 20:54:24 +0000

    Coin object

--- src/main/java/com/coinrank/Spider.java
+++ src/main/java/com/coinrank/Spider.java
@@ -1,7 +1,8 @@
 package com.coinrank;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -9,23 +10,36 @@ import com.google.gson.JsonObject;
 
 public class Spider
 {
+	private List<Coin> m_coins = new ArrayList<Coin>();
+	
+	private API m_api = new API();
+	
 	public Spider()
 	{
-		
+		try
+		{
+			JsonArray ticker = this.m_api.Ticker().Get().getAsJsonArray("data");
+			JsonObject tickerMore = this.m_api.TickerMore().Get().getAsJsonObject("data").getAsJsonObject("Data");
+			
+			for(JsonElement coin : ticker)
+			{
+				this.m_coins.add(new Coin((JsonObject) coin, tickerMore));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void crawl()
 	{
-		API api = new API();
-		
 		try
 		{
-			JsonArray ticker = api.Ticker().Get().getAsJsonArray("data");
-			JsonObject tickerMore = api.TickerMore().Get().getAsJsonObject("data").getAsJsonObject("Data");
+			JsonArray ticker = this.m_api.Ticker().Get().getAsJsonArray("data");
+			JsonObject tickerMore = this.m_api.TickerMore().Get().getAsJsonObject("data").getAsJsonObject("Data");
 			
 			for(JsonElement coin : ticker)
 			{
-				new Coin((JsonObject) coin, tickerMore);
+				this.m_coins.add(new Coin((JsonObject) coin, tickerMore));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
